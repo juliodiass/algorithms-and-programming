76a3469bd6b878526368cf7c254e9e524ae0245a
julio-diaass <99994595+juliodiass@users.noreply.github.com>
2026-09-06 23:45:45 -0300

    Update restaurant management system

--- python/projects/app.py
+++ python/projects/app.py
@@ -18,24 +18,32 @@ def finalizar_app():
     os.system("cls")
     print("Você saiu do aplicativo.\n")
 
-def opcao_menu(opcao_escolhida):
-    if opcao_escolhida == 1:
-        print("Você escolheu a opção 1: Cadastrar Restaurante")
+def opcao_invalida():
+    print("Opção inválida. Por favor, escolha uma opção válida.\n")
+    input("Pressione uma tecla para voltar ao menu.")
+    main()
 
-    elif opcao_escolhida == 2:
-        print("Você escolheu a opção 2: Listar Restaurante")
-    elif opcao_escolhida == 3:
-        print("Você escolheu a opção 3: Ativar Restaurante")
-    elif opcao_escolhida == 4:
-        finalizar_app()
-    else:
-        print("Opção inválida. Por favor, escolha uma opção válida.")
+def opcao_menu():
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
+        if opcao_escolhida == 1:
+            print("Você escolheu a opção 1: Cadastrar Restaurante")
 
+        elif opcao_escolhida == 2:
+            print("Você escolheu a opção 2: Listar Restaurante")
+        elif opcao_escolhida == 3:
+            print("Você escolheu a opção 3: Ativar Restaurante")
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 def main():
+    os.system("cls")
     exibir_nome_app()
     exibir_menu()
-    opcao_escolhida = int(input("Escolha uma opção: "))
-    opcao_menu(opcao_escolhida)
+    opcao_menu()
 
 if __name__ == "__main__":
 
