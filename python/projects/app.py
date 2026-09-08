5a7e5fa9fb247e4dc9f386388d110b773d92d593
julio-diaass <99994595+juliodiass@users.noreply.github.com>
2026-09-07 22:48:57 -0300

    Add restaurant registration

--- python/projects/app.py
+++ python/projects/app.py
@@ -1,4 +1,7 @@
 import os
+
+restaurantes = ["Sabor caseiro", "Cheiro e sabor"]
+
 def exibir_nome_app():
     print("""
     ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -23,14 +26,30 @@ def opcao_invalida():
     input("Pressione uma tecla para voltar ao menu.")
     main()
 
+def cadastrar_restaurante():
+    os.system("cls")
+    print("Cadastro de novos restaurantes\n")
+    nome_restaurante = input("Digite o nome do restaurante do qual deseja cadastrar: ")
+    restaurantes.append(nome_restaurante)
+    print(f"Restaurante {nome_restaurante} cadastrado com sucesso!\n")
+    input("Pressione uma tecla para voltar ao menu principal.")
+    main()
+
+def listar_restaurantes():
+    os.system("cls")
+    print("Listando os restaurantes cadastrados\n")
+    for restaurante in restaurantes:
+        print(f". {restaurante}")
+    input("\nPressione uma tecla para voltar ao menu principal.")
+    main()
+
 def opcao_menu():
     try:
         opcao_escolhida = int(input("Escolha uma opção: "))
         if opcao_escolhida == 1:
-            print("Você escolheu a opção 1: Cadastrar Restaurante")
-
+            cadastrar_restaurante()
         elif opcao_escolhida == 2:
-            print("Você escolheu a opção 2: Listar Restaurante")
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print("Você escolheu a opção 3: Ativar Restaurante")
         elif opcao_escolhida == 4:
