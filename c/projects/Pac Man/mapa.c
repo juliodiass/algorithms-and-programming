27b0ef690b066737961932a6231d42253132cef5
julio-diaass <99994595+juliodiass@users.noreply.github.com>
2026-09-25 17:27:48 -0300

    Use VAZIO constant in ehvazia

--- c/projects/Pac Man/mapa.c
+++ c/projects/Pac Man/mapa.c
@@ -12,13 +12,13 @@ int ehvalida(MAPA *m, int x, int y){
 }
 
 int ehvazia(MAPA *m, int x, int y){
-    return m->matriz[x][y] == '.';
+    return m->matriz[x][y] == VAZIO;
 }
 
 void andando(MAPA *m, int xorigem, int yorigem, int xdestino, int ydestino){
     char personagem = m->matriz[xorigem][yorigem];
     m->matriz[xdestino][ydestino] = personagem;
-    m->matriz[xorigem][yorigem] = '.';
+    m->matriz[xorigem][yorigem] = VAZIO;
 }
 
 void lermapa(MAPA* m){
